717ce73b797bc059333ef71fa1c416e0b44771cb
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2024-09-26 14:32:56 -0400

    Initial HTML, CSS transfer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+      <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Smit Sakariya
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,79 @@
+function HomePage() {
+  return (
+    <main className="center" id="main">
+        <section className="page-area">
+            <article className="h-entry">
+                <h2 className="p-name">Stay on track</h2>
+                <p className="e-content">
+                    Do you know where you are spending your money? If you really stop to track it down,
+                    you would get surprised! Proper budget management depends on real data... and this
+                    app will help you with that!
+                </p>
+            </article>
+    
+            <article className="h-entry">
+                <h2 className="p-name">Alerts</h2>
+                <p className="e-content">
+                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+                </p>
+            </article>
+    
+            <article className="h-entry">
+                <h2 className="p-name">Results</h2>
+                <p className="e-content">
+                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                    Also, they to live happier lives... since they expend without guilt or fear... 
+                    because they know it is all good and accounted for.
+                </p>
+            </article>
+    
+            <article className="h-entry">
+                <h2 className="p-name">Free</h2>
+                <p className="e-content">
+                    This app is free!!! And you are the only one holding your data!
+                </p>
+            </article>
+    
+            <article className="h-entry">
+                <h2 className="p-name">Stay on track</h2>
+                <p className="e-content">
+                    Do you know where you are spending your money? If you really stop to track it down,
+                    you would get surprised! Proper budget management depends on real data... and this
+                    app will help you with that!
+                </p>
+            </article>
+    
+            <article className="h-entry">
+                <h2 className="p-name">Alerts</h2>
+                <p className="e-content">
+                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+                </p>
+            </article>
+    
+            <article className="h-entry">
+                <h2 className="p-name">Results</h2>
+                <p className="e-content">
+                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                    Also, they to live happier lives... since they expend without guilt or fear... 
+                    because they know it is all good and accounted for.
+                </p>
+            </article>
+    
+            
+            <article className="h-entry">
+                <h2 className="p-name">Chart</h2>
+                <figure>
+                    <p>
+                        <canvas id="myChart" width="400" height="400"></canvas>
+                    </p>
+                </figure>
+            </article>
+
+            <div id="chart2"></div>
+        </section>
+
+    </main>
+  );
+}
+
+export default HomePage;
